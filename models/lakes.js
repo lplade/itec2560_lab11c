92ef8b474345d9740f78bb8a58b9d453d9d015dc
Larry Lade <lplade@users.noreply.github.com>
2016-04-19 02:39:02 -0500

    Main logic Adding time mostly works. Time format is shabby. Needs more validation

---
+++ models/lakes.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var lakeSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+		unique: true,
+		lowercase: true
+	},
+	runs: [{ //array of runs
+		timeSecs: Number, //store all run times in seconds
+		timeHuman: String, //convert to a human-readable string too
+		dateRun: {
+			type: Date,
+			default: Date.now
+		}
+	}]
+});
+
+var Lake = mongoose.model('Lake', lakeSchema);
+
+module.exports = Lake;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,98 @@
 var express = require('express');
 var router = express.Router();
 
+var Lake = require('../models/lakes.js');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	//Ask the schema to find all the runs
+	//Results provided via callback
+	Lake.find(function (err, lakeDocs) {
+		if (err) {
+			return next(err);
+		}
+		res.render('index', {
+			title: 'Lake Runner',
+			runList: lakeDocs,
+			error: req.flash('error')
+		});
+	});
 });
 
+router.post('/', function (req, res, next) {
+	console.log(req.body.name);
+	req.body.runs = [];
+
+	var newLake = Lake(req.body);
+	newLake.save(function (err) {
+		//Handle validation errors
+		if (err) {
+			if (err.name = "ValidationError") {
+				req.flash('error', 'Invalid data');
+				return res.redirect('/');
+			}
+			//Handle duplication errors
+			if (err.code == 11000) {
+				req.flash('error', 'A lake with that name already exists');
+				return res.redirect('/');
+			}
+			//Some other error
+			return next(err);
+		}
+		//If no error, lake created. Redirect
+		res.status(201); //created
+		return res.redirect('/'); //get the home page
+	});
+});
+
+router.post('/addRun', function (req, res, next) {
+	//Check if user provided a date
+	var newRun = req.body.dateRun;
+	if (!newRun || newRun == '') {
+		//TODO error message
+		return res.redirect('/');
+	}
+
+	//TODO validate times?
+
+	//Find run with this name, add the new date,
+	// convert the time to seconds, convert that time to a human readable
+	// string, and save it
+	Lake.findOne({name: req.body.name}, function (err, lake) {
+		if (err) {
+			return next(err)
+		}
+		//If no lake found, then send message to app error handle
+		if (!lake) {
+			return next(new Error('No lake found with name ' + req.body.name))
+		}
+
+		var newDate = req.body.dateRun;
+		var newTime = (req.body.timeMM * 60) + req.body.timeSS;
+		//convert it back from seconds anyway
+		//TODO expand this to handle hours
+/*		var timeInMinutes = Math.floor(newTime / 60);
+		var timeInSeconds = newTime % 60;
+		//TODO assert that these match submitted values?*/
+		var timeInMinutes = req.body.timeMM;
+		var timeInSeconds = req.body.timeSS; //workaround for now
+		//http://stackoverflow.com/questions/13859538/simplest-inline-method-to-left-pad-a-string
+		var timeString = ("000" + timeInMinutes).slice(-3) + ":" + ("00" + timeInSeconds).slice(-2);
+
+		var newRun = {
+			timeSecs: (timeInMinutes * 60 ) + timeInSeconds,
+			timeHuman: timeString,
+			dateRun: newDate
+		};
+
+		lake.runs.push(newRun);
+
+		lake.save(function (err) {
+			if (err) {
+				return next(err);
+			}
+			res.redirect('/'); //return home
+		});
+	});
+});
 module.exports = router;
